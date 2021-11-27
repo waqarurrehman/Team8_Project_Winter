c856493c5414a744cad23e6c98cfc5f35bd78054
Annalena Sailer <annalena.sailer@t-online.de>
2021-11-27 19:20:36 +0100

    updated goldstandard for bbe2fdb and added levenshtein comp

---
+++ src/main/java/com/team8/webdataintegration/winter/identityResolution/BookAuthorComparatorLevenshtein.java
@@ -0,0 +1,78 @@
+package com.team8.webdataintegration.winter.identityResolution;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import com.team8.webdataintegration.winter.model.Author;
+import com.team8.webdataintegration.winter.model.Book;
+
+import de.uni_mannheim.informatik.dws.winter.matching.rules.comparators.Comparator;
+import de.uni_mannheim.informatik.dws.winter.matching.rules.comparators.ComparatorLogger;
+import de.uni_mannheim.informatik.dws.winter.model.Correspondence;
+import de.uni_mannheim.informatik.dws.winter.model.Matchable;
+import de.uni_mannheim.informatik.dws.winter.model.defaultmodel.Attribute;
+import de.uni_mannheim.informatik.dws.winter.similarity.string.LevenshteinSimilarity;
+
+public class BookAuthorComparatorLevenshtein implements Comparator<Book, Attribute> {
+
+	private static final long serialVersionUID = 1L;
+    private	LevenshteinSimilarity sim = new LevenshteinSimilarity();
+	
+	private ComparatorLogger comparisonLog;
+
+	@Override
+	public double compare(Book record1, Book record2, Correspondence<Attribute, Matchable> schemaCorrespondence) {
+		
+		
+		List<Author> listauth1 = record1.getAuthors();
+		List<Author> listauth2 = record2.getAuthors();
+		List<Double> authorsimilarity = new ArrayList<Double>();
+		String book1Title = record1.getTitle();
+		String book2Title = record2.getTitle();
+		//run the comparison only when both both book have authors 
+		if(listauth1 != null && !listauth1.isEmpty() 
+				&& 
+			listauth2 != null && !listauth2.isEmpty()) {
+    	    
+			for(Author a :listauth1) {
+				for(Author b: listauth2) {
+					//run the similarity when both authors have name
+					if(a.getAuthor_name() != null && !a.getAuthor_name().isEmpty() 
+						&& 
+						b.getAuthor_name() != null && !b.getAuthor_name().isEmpty()) {
+						double similarity = sim.calculate(a.getAuthor_name().toLowerCase(), b.getAuthor_name().toLowerCase());
+						authorsimilarity.add(similarity);
+						
+						//if(this.comparisonLog != null){
+						//	this.comparisonLog.setComparatorName(getClass().getName());
+						//	this.comparisonLog.setRecord1Value(book1Title+"_"+a.getAuthor_name());
+						//	this.comparisonLog.setRecord2Value(book2Title+"_"+b.getAuthor_name());
+						//	this.comparisonLog.setSimilarity(Double.toString(similarity));
+						//}
+					} else {
+						authorsimilarity.add(0d);
+					}
+				}
+			}
+			// return max similarity that was found
+			return Collections.max(authorsimilarity);
+		}else {
+		  return 0;	
+		}
+	} 
+	
+	@Override
+	public ComparatorLogger getComparisonLog() {
+		return this.comparisonLog;
+	}
+
+	@Override
+	public void setComparisonLog(ComparatorLogger comparatorLog) {
+		this.comparisonLog = comparatorLog;
+	}
+
+	
+	
+
+}
