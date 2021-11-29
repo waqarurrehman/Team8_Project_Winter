4f30b49f71e1a2e6a90ad710415a39d6f55a3fac
Felix Krömer <kroemer.felix@gmail.com>
2021-11-29 22:15:37 +0100

    rename variables and fix typos

--- src/main/java/com/team8/webdataintegration/winter/BookUseCase.java
+++ src/main/java/com/team8/webdataintegration/winter/BookUseCase.java
@@ -11,15 +11,9 @@ import org.slf4j.Logger;
 
 import com.team8.webdataintegration.winter.datafusion.evaluation.*;
 import com.team8.webdataintegration.winter.datafusion.fusers.*;
-import com.team8.webdataintegration.winter.identityResolution.BookAuthorComparatorEqual;
-import com.team8.webdataintegration.winter.identityResolution.BookAuthorComparatorLowerJaccard;
-import com.team8.webdataintegration.winter.identityResolution.BookBlockingKeyByDecadeGenerator;
 import com.team8.webdataintegration.winter.identityResolution.BookBlockingKeyByTitleGenerator;
 import com.team8.webdataintegration.winter.identityResolution.BookCustomAuthorComparator;
 import com.team8.webdataintegration.winter.identityResolution.BookCustomTitleComparator;
-import com.team8.webdataintegration.winter.identityResolution.BookReleaseDateComparatorWeightedDateSimilarity;
-import com.team8.webdataintegration.winter.identityResolution.BookTitleComparatorEqual;
-import com.team8.webdataintegration.winter.identityResolution.BookTitleComparatorLowerJaccard;
 import com.team8.webdataintegration.winter.model.Book;
 import com.team8.webdataintegration.winter.model.BookXMLFormatter;
 import com.team8.webdataintegration.winter.model.BookXMLReader;
@@ -43,80 +37,76 @@ import de.uni_mannheim.informatik.dws.winter.model.RecordGroupFactory;
 import de.uni_mannheim.informatik.dws.winter.model.defaultmodel.Attribute;
 import de.uni_mannheim.informatik.dws.winter.model.io.CSVCorrespondenceFormatter;
 import de.uni_mannheim.informatik.dws.winter.processing.Processable;
-import de.uni_mannheim.informatik.dws.winter.utils.WinterLogManager;
 
 public class BookUseCase {
 	
-	private String sDS1_Path;
-	private String sDS2_Path;
-	private String sDS3_Path;
-	private String sIdentityResolution_GoldStandard_DS1_2_DS2;
-	private String sIdentityResolution_GoldStandard_DS2_2_DS3;
-	private String sCorrespondance_DS1_2_DS2_Path;
-	private String sCorrespondance_DS2_2_DS3_Path;
+	private String wikiSourcePath;
+	private String bbeSourcePath;
+	private String fdbSourcePath;
+	private String wikiBbeGoldstandardPath;
+	private String fdbBbeGoldstandardPath;
+	private String wikiBbeCorrespondencesPath;
+	private String fdbBbeCorrespondencesPath;
 	private String sXPath_Book;
 	private String sFusedXMLPath;
 	
 	
 	private static Logger logger = null;
 	
-	public BookUseCase(String dS1_Path, String dS2_Path,String dS3_Path,
-			String gs_DS1_DS2 , String gs_DS2_DS3 ,
-			String correspndanceDS1_DS2, String correspondanceDS2_DS3,
+	public BookUseCase(String wikiSourcePath, String bbeSourcePath,String fdbSourcePath,
+			String wikiBbeGoldstandardPath , String fdbBbeGoldstandardPath ,
+			String wikiBbeCorrespondencesPath, String fdbBbeCorrespondencesPath,
 			String XPath_Book,
 			String fusedXMLPath,
 			Logger logger
 			) {
 		
-		this.sDS1_Path = dS1_Path;
-		this.sDS2_Path = dS2_Path;
-		this.sDS3_Path = dS3_Path;
-		this.sIdentityResolution_GoldStandard_DS1_2_DS2 = gs_DS1_DS2;
-		this.sIdentityResolution_GoldStandard_DS2_2_DS3 = gs_DS2_DS3;
-		this.sCorrespondance_DS1_2_DS2_Path = correspndanceDS1_DS2;
-		this.sCorrespondance_DS2_2_DS3_Path = correspondanceDS2_DS3;
+		this.wikiSourcePath = wikiSourcePath;
+		this.bbeSourcePath = bbeSourcePath;
+		this.fdbSourcePath = fdbSourcePath;
+		this.wikiBbeGoldstandardPath = wikiBbeGoldstandardPath;
+		this.fdbBbeGoldstandardPath = fdbBbeGoldstandardPath;
+		this.wikiBbeCorrespondencesPath = wikiBbeCorrespondencesPath;
+		this.fdbBbeCorrespondencesPath = fdbBbeCorrespondencesPath;
 		this.sXPath_Book = XPath_Book;
 		this.sFusedXMLPath = fusedXMLPath;
 		this.logger = logger;
-		
-		
 	}
 	
 	public void RunIdentityResolution() throws Exception {
 		logger.info("Started  RunIdentityResolution Method Execution");
-		HashedDataSet<Book, Attribute> DS1 = new HashedDataSet<>();
-		HashedDataSet<Book, Attribute> DS2 = new HashedDataSet<>();
-		HashedDataSet<Book, Attribute> DS3 = new HashedDataSet<>();
-		logger.info("Loading Data Set 1 ["+this.sDS1_Path+"]");
-		new BookXMLReader().loadFromXML(new File(this.sDS1_Path), this.sXPath_Book, DS1);
-		logger.info("Loading Data Set 2 ["+this.sDS2_Path+"]");
-		new BookXMLReader().loadFromXML(new File(this.sDS2_Path),this.sXPath_Book, DS2);
-		logger.info("Loading Data Set 3 ["+this.sDS1_Path+"]");
-		new BookXMLReader().loadFromXML(new File(this.sDS3_Path), this.sXPath_Book, DS3);
+		HashedDataSet<Book, Attribute> wiki = new HashedDataSet<>();
+		HashedDataSet<Book, Attribute> bbe = new HashedDataSet<>();
+		HashedDataSet<Book, Attribute> fdb = new HashedDataSet<>();
+		logger.info("Loading Data Set 1 ["+this.wikiSourcePath +"]");
+		new BookXMLReader().loadFromXML(new File(this.wikiSourcePath), this.sXPath_Book, wiki);
+		logger.info("Loading Data Set 2 ["+this.bbeSourcePath +"]");
+		new BookXMLReader().loadFromXML(new File(this.bbeSourcePath),this.sXPath_Book, bbe);
+		logger.info("Loading Data Set 3 ["+this.wikiSourcePath +"]");
+		new BookXMLReader().loadFromXML(new File(this.fdbSourcePath), this.sXPath_Book, fdb);
 		
-		logger.info("Loading Gold Standard DS 1 to DS 2 ["+sIdentityResolution_GoldStandard_DS1_2_DS2+"]");
+		logger.info("Loading Gold Standard DS 1 to DS 2 ["+ wikiBbeGoldstandardPath +"]");
 		// load the gold standard (target_wiki 2 target_bbe)
-	    MatchingGoldStandard gsTest_DS1_DS2 = new MatchingGoldStandard();
-	    gsTest_DS1_DS2.loadFromCSVFile(new File(sIdentityResolution_GoldStandard_DS1_2_DS2));
+	    MatchingGoldStandard wikiBbeGoldstandard = new MatchingGoldStandard();
+	    wikiBbeGoldstandard.loadFromCSVFile(new File(wikiBbeGoldstandardPath));
 		
-	    logger.info("Loading Gold Standard DS 2 to DS 3 ["+sIdentityResolution_GoldStandard_DS2_2_DS3+"]");
+	    logger.info("Loading Gold Standard DS 2 to DS 3 ["+ fdbBbeGoldstandardPath +"]");
 		// load the gold standard (target_wiki 2 target_fdb)
-		MatchingGoldStandard gsTest_DS2_DS3 = new MatchingGoldStandard();
-		gsTest_DS2_DS3.loadFromCSVFile(new File(sIdentityResolution_GoldStandard_DS2_2_DS3));
+		MatchingGoldStandard fdbBbeGoldstandard = new MatchingGoldStandard();
+		fdbBbeGoldstandard.loadFromCSVFile(new File(fdbBbeGoldstandardPath));
 		
 		LinearCombinationMatchingRule<Book, Attribute> matchingRule = new LinearCombinationMatchingRule<>(
 				0.80);
-		matchingRule.activateDebugReport("usecase/books/output/debugResultsMatchingRule_wiki_2_bbe.csv", 1000, gsTest_DS1_DS2);
+		matchingRule.activateDebugReport("usecase/books/output/debugResultsMatchingRule_wiki_2_bbe.csv", 10000, wikiBbeGoldstandard);
 		
 		logger.info("Adding Title and Author Comparator");
-	   //	matchingRule.addComparator(new BookTitleComparatorLowerJaccard(), 0.70);
+	    // matchingRule.addComparator(new BookTitleComparatorLowerJaccard(), 0.70);
 		matchingRule.addComparator(new BookCustomTitleComparator(), 0.60);
 		
-		logger.info("Adding Authro and Author Comparator");
-		 //matchingRule.addComparator(new BookAuthorComparatorLowerJaccard(), 0.25);
+		logger.info("Adding Author and Author Comparator");
+		//matchingRule.addComparator(new BookAuthorComparatorLowerJaccard(), 0.25);
 		matchingRule.addComparator( new BookCustomAuthorComparator(), 0.40);
-		
-		//logger.info("Adding Authro and Release Date Comparator");
+
 		//matchingRule.addComparator(new BookReleaseDateComparatorWeightedDateSimilarity(), 0.05);
 		
 		logger.info("Adding Standard Record Blocker");
@@ -126,75 +116,68 @@ public class BookUseCase {
 		//Write debug results to file:
 		blocker.collectBlockSizeData("usecase/books/output/debugResultsBlocking.csv", 100);
 		
-		logger.info("Initilizaing the matching engine");
+		logger.info("Initializing the matching engine");
 		// Initialize Matching Engine
 		MatchingEngine<Book, Attribute> engine = new MatchingEngine<>();
 		
 		logger.info("Getting correspondence for wiki data and bbe dataset");
 		// Execute the matching for wiki and bbe dataset
-		Processable<Correspondence<Book, Attribute>> correspondences_DS1_DS2 = engine.runIdentityResolution(
-				DS1, DS2, null, matchingRule,
+		Processable<Correspondence<Book, Attribute>> wikiBbeCorrespondences = engine.runIdentityResolution(
+				wiki, bbe, null, matchingRule,
 				blocker);
 		
 		logger.info("Getting correspondence for wiki data and bbe dataset operation completed");
 		//updating the gold standard for wiki and wdc dataset
-		matchingRule.activateDebugReport("usecase/books/output/debugResultsMatchingRule_bbe_2_fdb.csv", 1000, gsTest_DS2_DS3);
+		matchingRule.activateDebugReport("usecase/books/output/debugResultsMatchingRule_bbe_2_fdb.csv", 10000, fdbBbeGoldstandard);
 		
 		// Execute the matching for wiki and bbe dataset
 		logger.info("Getting correspondence for BBE and FDB");
-		Processable<Correspondence<Book, Attribute>> correspondences_DS2_DS3 = engine.runIdentityResolution(
-				DS2, DS3, null, matchingRule,
+		Processable<Correspondence<Book, Attribute>> fdbBbeCorrespondences = engine.runIdentityResolution(
+				bbe, fdb, null, matchingRule,
 				blocker);
-		logger.info("Getting correspondence for BBE dataset and FDB  dataset operation completed");
+		logger.info("Getting correspondence for BBE dataset and FDB dataset operation completed");
 		
-		new CSVCorrespondenceFormatter().writeCSV(new File(sCorrespondance_DS1_2_DS2_Path), correspondences_DS1_DS2);
-		new CSVCorrespondenceFormatter().writeCSV(new File(sCorrespondance_DS2_2_DS3_Path), correspondences_DS2_DS3);
-		
-		
-		matchingRule.exportTrainingData(DS1,DS2,
-				gsTest_DS1_DS2, new File("usecase/books/output/optimisation/DS1_2_DS2_features.csv"));
-		matchingRule.exportTrainingData(DS2, DS3,
-				gsTest_DS2_DS3, new File("usecase/books/output/optimisation/DS2_2_DS3_features.csv"));
-		
+		new CSVCorrespondenceFormatter().writeCSV(new File(wikiBbeCorrespondencesPath), wikiBbeCorrespondences);
+		new CSVCorrespondenceFormatter().writeCSV(new File(fdbBbeCorrespondencesPath), fdbBbeCorrespondences);
+
 		// evaluate your result 
-		MatchingEvaluator<Book, Attribute> evaluator_wiki_bbe = new MatchingEvaluator<Book, Attribute>();
-		Performance perfTest_DS1_DS2 = evaluator_wiki_bbe.evaluateMatching(correspondences_DS1_DS2.get(),
-						gsTest_DS1_DS2);
+		MatchingEvaluator<Book, Attribute> wikiBbeEvaluator = new MatchingEvaluator<Book, Attribute>();
+		Performance wikiBbePerfTest = wikiBbeEvaluator.evaluateMatching(wikiBbeCorrespondences.get(),
+						wikiBbeGoldstandard);
 
 	    // print the evaluation result
-		logger.info(this.sDS1_Path +" <-> "+this.sDS2_Path);
-		logger.info(String.format("Precision: %.4f", perfTest_DS1_DS2.getPrecision()));
-		logger.info(String.format("Recall: %.4f", perfTest_DS1_DS2.getRecall()));
-		logger.info(String.format("F1: %.4f", perfTest_DS1_DS2.getF1()));
+		logger.info(this.wikiSourcePath +" <-> "+this.bbeSourcePath);
+		logger.info(String.format("Precision: %.4f", wikiBbePerfTest.getPrecision()));
+		logger.info(String.format("Recall: %.4f", wikiBbePerfTest.getRecall()));
+		logger.info(String.format("F1: %.4f", wikiBbePerfTest.getF1()));
 				
-		MatchingEvaluator<Book, Attribute> evaluator_wiki_WDC = new MatchingEvaluator<Book, Attribute>();
-		Performance perfTest_DS2_DS3 = evaluator_wiki_WDC.evaluateMatching(correspondences_DS2_DS3.get(),
-				gsTest_DS2_DS3);
+		MatchingEvaluator<Book, Attribute> fdbBbeEvaluator = new MatchingEvaluator<Book, Attribute>();
+		Performance fdbBbePerfTest = fdbBbeEvaluator.evaluateMatching(fdbBbeCorrespondences.get(),
+				fdbBbeGoldstandard);
 
 		// print the evaluation result
-		logger.info(this.sDS2_Path +" <-> "+this.sDS3_Path);
-		logger.info(String.format("Precision: %.4f", perfTest_DS2_DS3.getPrecision()));
-		logger.info(String.format("Recall: %.4f", perfTest_DS2_DS3.getRecall()));
-		logger.info(String.format("F1: %.4f", perfTest_DS2_DS3.getF1()));
-		
-		
+		logger.info(this.bbeSourcePath +" <-> "+this.fdbSourcePath);
+		logger.info(String.format("Precision: %.4f", fdbBbePerfTest.getPrecision()));
+		logger.info(String.format("Recall: %.4f", fdbBbePerfTest.getRecall()));
+		logger.info(String.format("F1: %.4f", fdbBbePerfTest.getF1()));
+
 		logger.info("Ended  RunIdentityResolution Method Execution");
 	}
 	
 	public void RunDataFusion() throws Exception {
 		
 		logger.info("Starting   RunDataFusion  Method Execution");
-		FusibleDataSet<Book, Attribute> ds1 = new FusibleHashedDataSet<>();
-		new BookXMLReader().loadFromXML(new File(this.sDS1_Path), this.sXPath_Book, ds1);
-		ds1.printDataSetDensityReport();
+		FusibleDataSet<Book, Attribute> wiki = new FusibleHashedDataSet<>();
+		new BookXMLReader().loadFromXML(new File(this.wikiSourcePath), this.sXPath_Book, wiki);
+		wiki.printDataSetDensityReport();
 
-		FusibleDataSet<Book, Attribute> ds2 = new FusibleHashedDataSet<>();
-		new BookXMLReader().loadFromXML(new File(this.sDS2_Path), this.sXPath_Book, ds2);
-		ds2.printDataSetDensityReport();
+		FusibleDataSet<Book, Attribute> bbe = new FusibleHashedDataSet<>();
+		new BookXMLReader().loadFromXML(new File(this.bbeSourcePath), this.sXPath_Book, bbe);
+		bbe.printDataSetDensityReport();
 
-		FusibleDataSet<Book, Attribute> ds3 = new FusibleHashedDataSet<>();
-		new BookXMLReader().loadFromXML(new File(this.sDS3_Path), this.sXPath_Book , ds3);
-		ds3.printDataSetDensityReport();
+		FusibleDataSet<Book, Attribute> fdb = new FusibleHashedDataSet<>();
+		new BookXMLReader().loadFromXML(new File(this.fdbSourcePath), this.sXPath_Book , fdb);
+		fdb.printDataSetDensityReport();
 		
 		/*ds1.setScore(3.0);
 		ds2.setScore(1.0);
@@ -209,16 +192,16 @@ public class BookUseCase {
 				        .parseDefaulting(ChronoField.SECOND_OF_MINUTE, 0)
 				        .toFormatter(Locale.ENGLISH);
 				
-		ds1.setDate(LocalDateTime.parse("2012-01-01", formatter));
-		ds2.setDate(LocalDateTime.parse("2010-01-01", formatter));
-		ds3.setDate(LocalDateTime.parse("2008-01-01", formatter));
+		wiki.setDate(LocalDateTime.parse("2012-01-01", formatter));
+		bbe.setDate(LocalDateTime.parse("2010-01-01", formatter));
+		fdb.setDate(LocalDateTime.parse("2008-01-01", formatter));
 		
 		
 		// load correspondences
 		logger.info("load correspondences");
 		CorrespondenceSet<Book, Attribute> correspondences = new CorrespondenceSet<>();
-		correspondences.loadCorrespondences(new File(this.sCorrespondance_DS1_2_DS2_Path),ds1, ds2);
-		correspondences.loadCorrespondences(new File(this.sCorrespondance_DS2_2_DS3_Path),ds2, ds3);
+		correspondences.loadCorrespondences(new File(this.wikiBbeCorrespondencesPath),wiki, bbe);
+		correspondences.loadCorrespondences(new File(this.fdbBbeCorrespondencesPath),bbe, fdb);
 
 		// write group size distribution
 		
