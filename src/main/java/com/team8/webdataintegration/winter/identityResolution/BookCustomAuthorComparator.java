210d4d3954bcad69f8d52d7ad6659038f7df9b4f
Felix Krömer <kroemer.felix@gmail.com>
2021-11-29 23:29:09 +0100

    compare each author in a record to each author in the other

--- src/main/java/com/team8/webdataintegration/winter/identityResolution/BookCustomAuthorComparator.java
+++ src/main/java/com/team8/webdataintegration/winter/identityResolution/BookCustomAuthorComparator.java
@@ -1,5 +1,6 @@
 package com.team8.webdataintegration.winter.identityResolution;
 
+import com.team8.webdataintegration.winter.model.Author;
 import com.team8.webdataintegration.winter.model.Book;
 import de.uni_mannheim.informatik.dws.winter.matching.rules.comparators.Comparator;
 import de.uni_mannheim.informatik.dws.winter.matching.rules.comparators.ComparatorLogger;
@@ -18,21 +19,36 @@ public class BookCustomAuthorComparator implements Comparator<Book, Attribute> {
 
 	@Override
 	public double compare(Book record1, Book record2, Correspondence<Attribute, Matchable> schemaCorrespondence) {
-		String author1 = record1.getAuthors().get(0).getAuthor_name().replaceAll("\\.|,", " ").toLowerCase().replaceAll(" +", " ");
-		String author2 = record2.getAuthors().get(0).getAuthor_name().replaceAll("\\.|,", " ").toLowerCase().replaceAll(" +", " ");
-		double sim = lev.calculate(author1, author2);
+		double sim;
+		double max = 0;
+		String max1 = normalizeAuthorName(record1.getAuthors().get(0).getAuthor_name());
+		String max2 = normalizeAuthorName(record2.getAuthors().get(0).getAuthor_name());
+		for(Author author1 : record1.getAuthors()) {
+			for(Author author2 : record2.getAuthors()){
+				sim = lev.calculate(normalizeAuthorName(author1.getAuthor_name()), normalizeAuthorName(author2.getAuthor_name()));
+				if(sim > max) {
+					max = sim;
+					max1 = normalizeAuthorName(author1.getAuthor_name());
+					max2 = normalizeAuthorName(author2.getAuthor_name());
+				}
+			}
+		}
 
 		if(comparisonLog != null) {
 			comparisonLog.setComparatorName(getClass().getName());
 			comparisonLog.setRecord1Value(record1.getAuthors().get(0).getAuthor_name());
 			comparisonLog.setRecord2Value(record2.getAuthors().get(0).getAuthor_name());
-			comparisonLog.setRecord1PreprocessedValue(author1);
-			comparisonLog.setRecord2PreprocessedValue(author2);
-			comparisonLog.setSimilarity(Double.toString(sim));
+			comparisonLog.setRecord1PreprocessedValue(max1);
+			comparisonLog.setRecord2PreprocessedValue(max2);
+			comparisonLog.setSimilarity(Double.toString(max));
 		}
 
-		return sim;
-	} 
+		return max;
+	}
+
+	public static String normalizeAuthorName(String name) {
+		return name.replaceAll("\\.|,", " ").toLowerCase().replaceAll(" +", " ");
+	}
 	
 	@Override
 	public ComparatorLogger getComparisonLog() {
